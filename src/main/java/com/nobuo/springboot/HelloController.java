80bae1bf54954813e7e758cf8c9d829886bfb0e6
Iga <gonnbe1106@yahoo.co.jp>
2019-04-10 22:16:25 +0900

    Thymeleaf

--- src/main/java/com/nobuo/springboot/HelloController.java
+++ src/main/java/com/nobuo/springboot/HelloController.java
@@ -2,45 +2,62 @@ package com.nobuo.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class HelloController {
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping(value="/")
+	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "フォームを送信してください。");
+		mav.addObject("id", id);
+		mav.addObject("check", id % 2 == 0);
+		mav.addObject("trueVal", "Even number!");
+		mav.addObject("falseVal", "Odd number...");
 		return mav;
 	}
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView send(
-		@RequestParam(value="check1",required=false)boolean check1, 
-		@RequestParam(value="radio1",required=false)String radio1,
-		@RequestParam(value="select1",required=false)String select1,
-		@RequestParam(value="select2",required=false)String[] select2,
-		ModelAndView mav) {
 		
-		String res = "";
-		try {
-			res = "check:" + check1 +
-				  " radio:" + radio1 +
-				  " select:" + select1 +
-				  "\nselect/2:";
-		} catch (NullPointerException e) {}
-		try {
-			res += select2[0];
-			for(int i = 1;i < select2.length;i++)
-				res += ", " + select2[i]; 
-				} catch (NullPointerException e) {
-				res += "null";
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "current data.");
+		DataObject obj = new DataObject(123, "hanako", "hanako@lower");
+		mav.addObject("object", obj);
+		return mav;
+	}
+	@RequestMapping(value="/other")
+	public String other() {
+		return "redirect:/";
+	}
+	@RequestMapping("/home")
+	public String home() {
+		return "forward:";
+	}
+	class DataObject {
+		private int id;
+		private String name;
+		private String value;
+		
+		public DataObject(int id, String name, String value) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.value = value;
+		}
+	
+		public int getId() { return id; }
+		public void setId(int id) { this.id = id; }
+		public String getName() {return name; }
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getValue() {
+			return value;
+		}
+		public void setValue(String value) {
+			this.value = value;
+			
 		}	
-			mav.addObject("msg", res);
-			mav.setViewName("index");
-			return mav;
-		
 	}
 }
-		
+				
